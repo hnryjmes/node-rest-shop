b1f4d403af569d8bea8ad1609bd05d8b372ebf00
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-17 10:33:23 +0000

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import * as http from "http";
+import app from "./app";
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(port);
