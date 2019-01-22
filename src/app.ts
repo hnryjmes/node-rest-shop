448ddeab2fe2f127ac49b7e7dcc10a60eadab8ac
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-22 16:38:29 +0000

    Add morgan, error messages

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 import express from "express";
 const app = express();
+import morgan from "morgan";
 
 import orderRoutes from "./api/routes/orders";
 import productRoutes from "./api/routes/products";
 
+app.use(morgan("dev"));
+
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  next(error);
+});
+
+app.use((error: any, req: any, res: any, next: any) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 export default app;
